b0e2172353c21ea51dad60b76ed8669fbfdd0f87
jcl <ljc135789@163.com>
2020-04-19 14:52:54 +0800

    在线教育平台

--- jiaoyu/mxonline/apps/courses/adminx.py
+++ jiaoyu/mxonline/apps/courses/adminx.py
@@ -7,11 +7,49 @@
 @Software: PyCharm
 """
 import xadmin
-from apps.courses.models import Course,Lesson,Video,CourseResource
+from xadmin import views
+from apps.courses.models import Course, Lesson, Video, CourseResource
 
 
-class CoursesAadmin():
-    pass
+class GlobalSettings(object):
+    site_title = "柳园G"
+    site_footer = "柳园G413"
+    # menu_style = "accordion"
+
+class BaseSettings(object):
+    enable_themes = True
+    use_bootswatch = True
 
 
-xadmin.site.register(Course, CoursesAadmin)
+class CourseAdmin(object):
+    list_display = ['name', 'desc', 'detail', 'degree', 'learn_times', 'students', 'teacher']
+    search_fields = ['name', 'desc', 'detail', 'degree', 'students']
+    list_filter = ['name', 'teacher__name', 'desc', 'detail', 'degree', 'learn_times', 'students']
+    list_editable = ["degree", "desc"]
+
+
+class LessonAdmin(object):
+    list_display = ['course', 'name', 'add_time']
+    search_fields = ['course', 'name']
+    list_filter = ['course__name', 'name', 'add_time']
+
+
+class VideoAdmin(object):
+    list_display = ['lesson', 'name', 'add_time']
+    search_fields = ['lesson', 'name']
+    list_filter = ['lesson', 'name', 'add_time']
+
+
+class CourseResourceAdmin(object):
+    list_display = ['course', 'name', 'file', 'add_time']
+    search_fields = ['course', 'name', 'file']
+    list_filter = ['course', 'name', 'file', 'add_time']
+
+
+xadmin.site.register(Course, CourseAdmin)
+xadmin.site.register(Lesson, LessonAdmin)
+xadmin.site.register(Video, VideoAdmin)
+xadmin.site.register(CourseResource, CourseResourceAdmin)
+
+xadmin.site.register(xadmin.views.CommAdminView, GlobalSettings)
+xadmin.site.register(xadmin.views.BaseAdminView, BaseSettings)
--- jiaoyu/mxonline/apps/organizations/adminx.py
+++ jiaoyu/mxonline/apps/organizations/adminx.py
@@ -11,16 +11,25 @@ from apps.organizations.models import Teacher,CourseOrg,City
 
 
 class TeacherAadmin(object):
-    pass
+    list_display = ['org', 'name', 'work_years', 'work_company']
+    search_fields = ['org', 'name', 'work_years', 'work_company']
+    list_filter = ['org', 'name', 'work_years', 'work_company']
+
 
 class CourseOrgAadmin(object):
-    pass
+    list_display = ['name', 'desc', 'click_nums', 'fav_nums']
+    search_fields = ['name', 'desc', 'click_nums', 'fav_nums']
+    list_filter = ['name', 'desc', 'click_nums', 'fav_nums']
+    style_fields = {
+        "desc": "ueditor"
+    }
+
 
 class CityAadmin(object):
-    list_display = ['id','name','desc']
-    search_fields = ['name','desc']
-    list_filter = ['name','desc','add_time']
-    list_editable = ['name','desc']
+    list_display = ["id", "name", "desc"]
+    search_fields = ["name", "desc"]
+    list_filter = ["name", "desc", "add_time"]
+    list_editable = ["name", "desc"]
 
 
 
