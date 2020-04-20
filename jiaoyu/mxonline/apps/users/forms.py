e40e469b75d700871d66ef59d33df478ec575ff8
jcl <ljc135789@163.com>
2020-04-20 21:45:14 +0800

    在线教育平台

--- jiaoyu/mxonline/apps/users/forms.py
+++ jiaoyu/mxonline/apps/users/forms.py
@@ -7,7 +7,14 @@
 @Software: PyCharm
 """
 from django import forms
+from captcha.fields import CaptchaField
 
 class LoginForm(forms.Form):
     username = forms.CharField(required=True, min_length=2)
     password = forms.CharField(required=True, min_length=3)
+
+class DynamicLoginForm(forms.Form):
+    captcha = CaptchaField()
+
+class DynamicRegisterForm(forms.Form):
+    captcha = CaptchaField()
