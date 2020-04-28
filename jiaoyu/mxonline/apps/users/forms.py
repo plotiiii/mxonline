f82972c6bf15a773fd8a97cf534ae96bbed54e16
jcl <ljc135789@163.com>
2020-04-28 20:36:08 +0800

    在线教育平台

--- jiaoyu/mxonline/apps/users/forms.py
+++ jiaoyu/mxonline/apps/users/forms.py
@@ -6,15 +6,45 @@
 @File : forms.py
 @Software: PyCharm
 """
-from django import forms
 from captcha.fields import CaptchaField
+from django import forms
+
+from apps.users.models import UserProfile
+
+
+class ChangePwdForm(forms.Form):
+    password1 = forms.CharField(required=True, min_length=5)
+    password2 = forms.CharField(required=True, min_length=5)
+
+    def clean(self):
+        pwd1 = self.cleaned_data["password1"]
+        pwd2 = self.cleaned_data["password2"]
+
+        if pwd1 != pwd2:
+            raise forms.ValidationError("密码不一致")
+        return self.cleaned_data
+
+
+class UserInfoForm(forms.ModelForm):
+    class Meta:
+        model = UserProfile
+        fields = ["nick_name", "gender", "birthday", "address"]
+
+
+class UploadImageForm(forms.ModelForm):
+    class Meta:
+        model = UserProfile
+        fields = ["image"]
+
 
 class LoginForm(forms.Form):
     username = forms.CharField(required=True, min_length=2)
     password = forms.CharField(required=True, min_length=3)
 
+
 class DynamicLoginForm(forms.Form):
     captcha = CaptchaField()
+
 
 class DynamicRegisterForm(forms.Form):
     captcha = CaptchaField()
